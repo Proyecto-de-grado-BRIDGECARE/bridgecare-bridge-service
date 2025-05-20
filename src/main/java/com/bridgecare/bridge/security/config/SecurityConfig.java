ce59e9c254576f5541c8a34a77b774fa44fe88d9
Daniel Vargas <dvargasp@javeriana.edu.co>
2025-05-20 20:56:02 +0100

    Add missing imports

--- src/main/java/com/bridgecare/bridge/security/config/SecurityConfig.java
+++ src/main/java/com/bridgecare/bridge/security/config/SecurityConfig.java
@@ -1,11 +1,16 @@
 package com.bridgecare.bridge.security.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.bridgecare.bridge.security.services.JwtAuthenticationFilter;
 import com.bridgecare.bridge.security.services.JwtTokenProvider;
